67c6cd9b724f496ba1c61506edb3268e8ebca96f
user_11_20P1 <20p1_11@mail.ru>
2025-01-28 16:51:25 +0500

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styles from './styles/index.module.scss';
+
+export const App = () =>{
+  return(
+    <div>
+      <div className={`${styles.btn} ${styles.lg}`}>large</div>
+      <br />
+      <div className={`${styles.btn} ${styles.md}`}>medium</div>
+      <br />
+      <div className={`${styles.btn} ${styles.sm}`}>small</div>
+
+    </div>
+  );
+}
